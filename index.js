f88e8145710f7ccd17b8aae7fb01d6ae40b9d5de
AJMANZ <113150864+AJManzione@users.noreply.github.com>
2022-10-21 20:16:59 -0600

    added fs, working html being generated

--- index.js
+++ index.js
@@ -25,8 +25,30 @@ console.log(memberOne);
 
 
 
+const template =`
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>HELLO WORLD</p>
+</body>
+</html>
+`;
 
+    createHtmlFile(template)
+});
 
+function createHtmlFile(data) {
 
-
+// Filesave code 
+fs.writeFile(`./index.html`, data, (err)=>{
+    if (err){
+        console.log(err)
+    } console.log("HTML Generated");
 })
+}
