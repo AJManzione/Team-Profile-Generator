9f166263045be13d0ed9b8cf83251cd7b72d45a2
AJMANZ <113150864+AJManzione@users.noreply.github.com>
2022-10-23 20:01:01 -0600

    working program, needs testing

--- index.js
+++ index.js
@@ -3,19 +3,18 @@ const fs = require('fs')
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const { Console } = require('console');
-const internal = require('stream');
 
 const team = [];
+let htmlTemplate = '';
 
 promptManager()
 
 
 
 function CreateTeam() {
-
-console.log(team)
-
+ 
+htmlTemplate = htmlTemplate + ` </body>
+</html>`
 inquirer.prompt(
     [
         {
@@ -25,140 +24,15 @@ inquirer.prompt(
     ]
 )
 
-/* const engineerName = team.map(element => element.Engineer)
-console.log(engineersName); */
+data = htmlTemplate;
+
+    fs.writeFile('index.html', data , function (err) {
+        if (err) throw err;
+        console.log('File is created successfully.');
+      });
 
 }
 
-/* 
-.then(({
-    
-
-
-}) => {
-team.forEach(getManagerInfo);
-function getManagerInfo() {
-    const employeeName = team[0];
-    managerName = employeeName.name;
-    const employeeID = team[0];
-    managerID = employeeID.id;
-    const employeeEmail = team[0];
-    managerEmail = employeeEmail.email;
-    const employeeNum = team[0];
-    managerOfficeNum = employeeNum.officeNum;
-}
-
-team.forEach(getEngineerInfo);
-function getEngineerInfo() {
-    for (i = 1; i < team.length - 1; i++) {
-        const employee = employee + [i]
-        employee[i] = team[i]
-    if(!employee[i]) {
-        return;
-    } else { 
-        const employeeName = employeeName[i];
-        employeeName[i] = team[i];
-        engineerName[i] = employeeName[i].name;
-
-        const employeeID = employeeID[i];
-        employeeID[i] = team[i];
-        engineerId[i] = employeeID[i].id;
-
-        const employeeEmail = employeeEmail[i];
-        employeeEmail[i] = team[i];
-        engineerEmail[i] = employeeEmail[i].email;
-
-        const employeeGithub = employeeGithub[i];
-        employeeGithub[i] = team[i];
-        engineerGithub[i] = employeeGithub[i].github;
-    }}
-}
-
-team.forEach(getInternInfo);
-function getInternInfo() {
-    const employeeThree = team[2];
-    internName = employeeThree.name;
-    const employeeThreeID = team[2];
-    internId = employeeThreeID.id;
-    const employeeThreeEmail = team[2];
-    internEmail = employeeThreeEmail.email;
-    const employeeThreeSchool = team[2];
-    internSchool = employeeThreeSchool.school
-}
-
-
-
-
-
-    const internTemplate = `  
-    <div class="card" style="width: 18rem;">
-        <div class="card-header">
-        ${internName} <br>
-        Intern
-        </div>
-        <ul class="list-group list-group-flush">
-            <li class="list-group-item">ID: ${internId}</li>
-            <li class="list-group-item" id="link">Email:<a href="mailto:${internEmail}">${internEmail}</a></li>
-            <li class="list-group-item">School: ${internSchool}</li>
-        </ul>
-    </div>`
-
-    const engineerTemplate = `  
-    <div class="card" style="width: 18rem;">
-        <div class="card-header">
-        ${engineerName} <br>
-        Engineer
-        </div>
-        <ul class="list-group list-group-flush">
-            <li class="list-group-item">ID: ${engineerId}</li>
-            <li class="list-group-item" id="link">Email:<a href="mailto:${engineerEmail}">${engineerEmail}</a></li>
-            <li class="list-group-item">GitHub: ${engineerGithub}</li>
-        </ul>
-    </div>`
-
-    const managerTemplate = `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <title>Document</title>
-</head>
-<body>
-    <div class="card" style="width: 18rem;">
-        <div class="card-header">
-        ${managerName} <br>
-        Manager
-        </div>
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item">ID: ${managerID}</li>
-          <li class="list-group-item" id="link">Email:<a href="mailto:${managerEmail}">${managerEmail}</a></li>
-          <li class="list-group-item">Office #: ${managerOfficeNum}</li>
-        </ul>
-      </div>
-      ${engineerTemplate}
-      ${internTemplate}
-</body>
-</html>
-`
-    createIndexHtml(managerTemplate)
-    
-    function createIndexHtml(data) {
-        fs.writeFile("index.html", data, (err) => {
-            if (err) {
-                console.log(err)
-            } else {
-                console.log("index.html generated!")
-            }
-        })
-    }
-})
-};
-
- */
-/* ------------------------------------------------------------------------------------------------------------------------------------------- */
 
 function newTeamMember() {
     inquirer.prompt(
@@ -227,6 +101,7 @@ function promptManager() {
     managers.push(newManager.getEmail())
     managers.push(newManager.getOfficeNum())
     team.push(managers);
+    htmlTemplate = newManager.getTemplate()
 
     if(answer.newTeam == true) {
         newTeamMember()
@@ -283,6 +158,7 @@ function promptEngineer() {
         engineers.push(newEngineer.getEmail())
         engineers.push(newEngineer.getGithub())
         team.push(engineers)
+        htmlTemplate = htmlTemplate + newEngineer.getTemplate()
 
 
         if(answer.newTeam == true) {
@@ -338,6 +214,8 @@ function promptIntern() {
         interns.push(newIntern.getEmail())
         interns.push(newIntern.getSchool())
         team.push(interns);
+        team.push(managers);
+        htmlTemplate = htmlTemplate + newIntern.getTemplate()
 
         if(answer.newTeam == true) {
             newTeamMember()
