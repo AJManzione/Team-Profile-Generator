e9241a6f27995b0a5e8e86c6d108c60d9d56190a
AJMANZ <113150864+AJManzione@users.noreply.github.com>
2022-10-25 11:08:37 -0600

    added more test cases for engineer

--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -1,5 +1,23 @@
 const Engineer = require('../lib/Engineer')
 
+test('gets the engineer name', () => {
+    const engineer = new Engineer('Anthony', '5', 'gmail', 'ajmanzione')
+
+    expect(engineer.name).toBe('Anthony')
+})
+
+test('gets the engineer ID', () => {
+    const engineer = new Engineer('Anthony', '5', 'gmail', 'ajmanzione')
+
+    expect(engineer.id).toBe('5')
+})
+
+test('gets the engineer email', () => {
+    const engineer = new Engineer('Anthony', '5', 'gmail', 'ajmanzione')
+
+    expect(engineer.email).toBe('gmail')
+})
+
 test('gets the engineer github', () => {
     const engineer = new Engineer('Anthony', '5', 'gmail', 'ajmanzione')
 
